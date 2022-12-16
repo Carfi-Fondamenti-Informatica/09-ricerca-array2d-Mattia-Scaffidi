6c92b80fbc5ce422acd9b82b4ea3fc6e864e9688
Mattia-Scaffidi <116498876+Mattia-Scaffidi@users.noreply.github.com>
2022-12-16 21:50:12 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+bool controllo(char nomi[10][20], char nome[20], int &c){
 
+    for (int n = 0; n < 10; n++)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            if (nomi [n][i] != nome[i]){
+                i = 20;
+            }
+            if (i == 19){
+                c = n;
+                return true;
+            }
+        }
+    }
+    return false;
+}
